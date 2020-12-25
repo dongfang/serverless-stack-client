20c18558e3a9986583c47a15070c65a6d559a685
Soren Kuula <dongfang@Sorens-MacBook-Pro.local>
2020-12-25 17:17:21 +0100

    more shit

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -22,44 +22,42 @@ export default function Settings() {
         });
     }
 
-    return (
-        async function handleFormSubmit(storage, { token, error }) {
-            if (error) {
-                onError(error);
-                return;
-            }
-
-            setIsLoading(true);
-
-            try {
-                await billUser({
-                    storage,
-                    source: token.id
-                });
-
-                alert("Your card has been charged successfully!");
-                history.push("/");
-            } catch (e) {
-                onError(e);
-                setIsLoading(false);
-            }
+    async function handleFormSubmit(storage, { token, error }) {
+        if (error) {
+            onError(error);
+            return;
         }
 
-        return (
-                <div className="Settings">
-                <StripeProvider stripe={stripe}>
-                <Elements
-            fonts={[
-                {
-                    cssSrc:
-                    "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800",
-                },
-            ]}
-                >
-                <BillingForm isLoading={isLoading} onSubmit={handleFormSubmit} />
-                </Elements>
-                </StripeProvider>
-                </div>
-        );
+        setIsLoading(true);
+
+        try {
+            await billUser({
+                storage,
+                source: token.id
+            });
+
+            alert("Your card has been charged successfully!");
+            history.push("/");
+        } catch (e) {
+            onError(e);
+            setIsLoading(false);
+        }
+    }
+
+    return (
+            <div className="Settings">
+            <StripeProvider stripe={stripe}>
+            <Elements
+        fonts={[
+            {
+                cssSrc:
+                "https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800",
+            },
+        ]}
+            >
+            <BillingForm isLoading={isLoading} onSubmit={handleFormSubmit} />
+            </Elements>
+            </StripeProvider>
+            </div>
     );
 }
